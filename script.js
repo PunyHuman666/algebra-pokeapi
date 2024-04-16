8adb82237cc8244fa215dc723bd70ba3daaa1b99
PunyHuman666 <geutom335@gmail.com>
2024-04-16 12:32:03 +0200

    dodan nešto bolji style

---
+++ script.js
@@ -0,0 +1,27 @@
+const ul = document.getElementById("ul-template");
+const button = document.querySelector("button");
+
+async function pokemon() {
+  ul.innerHTML = "";
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon-color/10/");
+  const pokemons = await response.json();
+  let source = document.getElementById("template").innerHTML;
+  let template = Handlebars.compile(source);
+
+  for (let i = 0; i < 20; i++) {
+    const getId = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemons.pokemon_species[i].name}`
+    );
+    const pokemonId = await getId.json();
+    const context = {
+      id: pokemonId.id,
+      name: pokemons.pokemon_species[i].name.toUpperCase(),
+      url: pokemons.pokemon_species[i].url,
+      sprite: pokemonId.sprites.front_default,
+    };
+    ul.innerHTML += template(context);
+  }
+}
+
+button.addEventListener("click", pokemon);
+// pokemon();
